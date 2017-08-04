f82d04b2bf6e16ae7639f24d2997ed2d8168ed0f
Becky Hall <uncoolplane@gmail.com>
2017-08-03 19:10:36 -0600

    initial commit part 1

--- part1/assessment-part1.js
+++ part1/assessment-part1.js
@@ -44,26 +44,53 @@ function daBears(){
 // (Delete wrong answers, leave correct ones)
 
 var fairyTale1 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+//mamabear ... inherit from papaBear
+//babyBear ... overrides
+//goldilocks ... not accessible
+fairyTale1 = new Array();
+fairyTale1.push('papaBear');
+fairyTale1.push('mamaBear');
 
 // Which function(s) access the "feeling" variable and get "Hungry"
 // (Delete wrong answers, leave correct ones)
 
 var fairyTale2 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+//only goldilocks has this local variables, others undefined
+fairyTale2 = new Array();
+fairyTale2.push('goldilocks');
 
 // Which function(s) access the "porridge" variable and get "Too Cold!"
 // (Delete wrong answers, leave correct ones)
 
 var fairyTale3 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+//all have the variable .. papaBear defines
+//mamabear overwrites with 'Too Cold' ... correct value
+//babyBear overwrite the same variable
+//doesn't exist with goldilocks
+fairyTale3 = new Array();
+fairyTale3.push('mamaBear');
 
 // Which function(s) access the "sleepy" variable and get undefined
 // (Delete wrong answers, leave correct ones)
 
 var fairyTale4 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+//sleepy is a private variable accessible only to babyBear
+fairyTale4 = new Array();
+fairyTale4.push('papaBear');
+fairyTale4.push('mamaBear');
+fairyTale4.push('goldilocks');
+fairyTale4.push('daBears');
 
 // Which function(s) access the isFurry variable and get true
 // (Delete wrong answers, leave correct ones)
 
 var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+//isFurry is private variable for papaBear but accessible to functions contained within
+fairyTale5 = new Array();
+fairyTale5.push('papaBear');
+fairyTale5.push('mamaBear');
+fairyTale5.push('babyBear');
+fairyTale5.push('daBears');
 
 
 // *************
@@ -83,16 +110,26 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 // CODE HERE...
 
+function Vehicle(name) {
+  this.name = name;
+  this.gasRemaining = 100;
+}
+Vehicle.prototype.drive = function () {
+  this.gasRemaining -= 25;
+  console.log('driving ', this.name, this.gasRemaining);
+}
+var charger = new Vehicle('charger')
+var mustang = new Vehicle('mustang');
 
-
-
+charger.drive();
+mustang.drive();
+mustang.drive();
 
 // -----------------------------------------------------------------------------
 
 // *************
 // * PROBLEM 3 *
 // *************
-
 
 // For this problem, you will need to add a method to the String prototype named
 // "grammarPolice".  When called on a string, "grammarPolice" will return a new string
@@ -104,16 +141,20 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // Your method may be passed punctuation, numbers or other non-letter characters
 // and should neither modify them nor break when encountering them.
 
-
-
-
 // CODE HERE...
+function grammarPolice() {
+   return this.replace(/\w\S*/g,
+     function(txt){
+       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+     });
+}
 
+String.prototype.grammarPolice = grammarPolice;
 
 
 // *************
 // * PROBLEM 4 *
-// *************
+//*************
 
 // Write a function called valueType that has two parameters. Jasmine will pass
 // arguments into your function through the parameters.  Your function will need
@@ -126,8 +167,29 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // In all other cases, return "Different values"
 
 // CODE HERE...
+function valueType(p1, p2) {
+  var sameValue = false;
+  var sameType = false;
 
+  if(p1 == p2) {
+    sameValue = true;
+  }
+  if(typeof p1 == typeof p2) {
+    sameType = true;
+  }
 
+  if(sameValue && sameType) {
+    return 'Exactly the same';
+  } else if (sameValue==true && sameType == false) {
+    return 'Same value, different types';
+  } else {
+    return 'Different values'
+  }
+}
+
+console.log(valueType(2, 2));
+console.log(valueType(5, "5"));
+console.log(valueType(7, "Strawberry"));
 
 // *************
 // * PROBLEM 5 *
@@ -141,3 +203,8 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 var theAnswer = "Unknown";
 
 // CODE HERE...
+promiseCatcher = function(p1) {
+  p1().then(function (result) {
+    theAnswer = result;
+  })
+}
